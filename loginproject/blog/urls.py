fbc89cefaa1486de1e853d8802ffbdfb39a8bd23
tada <imjunghee@imjeonghuiui-MacBook-Pro.local>
2019-11-05 00:27:05 +0900

    01 장고 프로젝트 생성 및 기본 프로젝트 구성

---
+++ loginproject/blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+]
