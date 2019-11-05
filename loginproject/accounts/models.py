1cd4c910496b2ce28f359ada3531811510335256
tada <imjunghee@imjeonghuiui-MacBook-Pro.local>
2019-11-05 12:35:24 +0900

    06 가입과 동시에 Profile 만들기

--- loginproject/accounts/models.py
+++ loginproject/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save  # 회원가입과 동시에 프로파일 만들기
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    website_url = models.URLField(blank=True)
+
+def on_post_save_for_user(sender, **kwargs):  # kwargs : instance, created 인자 포함
+    if kwargs['created']:
+        user = kwargs['instance']
+        Profile.objects.create(user=user)
+
+post_save.connect(on_post_save_for_user, settings.AUTH_USER_MODEL)
